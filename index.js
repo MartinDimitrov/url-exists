34aeabaa47764aaaa38736de0a1641059f27d10c
Martin Dimitrov <marto_dimitrov@mail.bg>
2017-08-24 00:02:53 +0300

    Fix
    You missed the error printing if it failed :)

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var request = require('request');
 
 function urlExists(url, cb) {
   request({ url: url, method: 'HEAD' }, function(err, res) {
-    if (err) return cb(null, false);
+    if (err) return cb(err, false);
     cb(null, /4\d\d/.test(res.statusCode) === false);
   });
 }
